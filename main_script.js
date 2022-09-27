43dfdceb63740c2d771742ea7b60cee56a0c167b
Vishal <vc57813@gmail.com>
2022-09-27 18:45:03 +0530

    first commit

---
+++ main_script.js
@@ -0,0 +1,51 @@
+// * {
+//     box-sizing: border-box;
+// }
+
+// body {
+//     font-family:Verdana, Geneva, Tahoma, sans-serif;
+//     margin: 15px 30px;
+//     font-size: 17px;
+//     padding:8px;
+// }
+
+// .container {
+//     background-color: #f2f2f2;
+//     padding: 5px 20px 15px 20px;
+//     border-radius: 4px;
+// }
+
+// input[type="text"],
+// input[type="email"],
+// input[type="number"],
+// input[type="password"],
+// input[type="date"], select, textarea {
+//     width: 100%;
+//     padding: 12px;
+//     border:1px solid #ccc;
+//     border-radius:5px;
+// }
+
+// fieldset {
+//     border:1px solid #ccc;
+//     border-radius: 5px;
+// }
+
+// #main_head {
+//     text-align:center;
+// }
+
+// input[type="submit"] {
+//     background-color: rgb(13, 160, 79);
+//     border: none;
+//     padding: 10px;
+//     width: 100%;
+//     border-radius: 5px;
+//     transition: 300ms;
+// }
+
+// input[type="submit"]:hover {
+//     cursor: pointer;
+//     background-color: rgb(6, 141, 107);
+    
+// }
